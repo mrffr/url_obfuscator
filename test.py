ffa6613eb4e3b05a0e7efa07a96e974d1c3b3e31
F.Fitzgerald <mrffitzgerald@runbox.com>
2018-07-24 16:07:34 +0100

    replace char with hex, oct and hex conversions

--- test.py
+++ test.py
@@ -1,10 +1,31 @@
+#!/usr/bin/python3
 import unittest
 import url_obfuscator
+from random import seed
 
 class TestUrlObfus(unittest.TestCase):
     def test_ip(self):
-        self.assertEqual(url_obfuscator.ipv4_lookup("www.facebook.oneonestkst")[0], 2)
-        self.assertEqual(url_obfuscator.ipv4_lookup("www.facebook.com")[1], "31.13.73.39")
+        self.assertEqual(url_obfuscator.ip_lookup("www.github.oneonsot"), "IP error")
+
+    def test_ip_octal(self):
+        ip = "127.0.0.1".split('.')
+        ip = '.'.join([url_obfuscator.to_oct(int(i)) for i in ip])
+        self.assertEqual(ip, "0177.00.00.01")
+
+    def test_ip_hex(self):
+        ip = "127.0.0.1".split('.')
+        ip = '.'.join([url_obfuscator.to_hex(int(i)) for i in ip])
+        self.assertEqual(ip, "0x7f.0x00.0x00.0x01")
+
+    def test_dummy_auth(self):
+        seed(1)
+        self.assertEqual(url_obfuscator.dummy_auth(), "i)A0@")
+
+    def test_hext_obfus(self):
+        self.assertEqual(url_obfuscator.hext_obfus("////"), "////")
+        seed(1)
+        self.assertEqual(url_obfuscator.hext_obfus("/test_string.html"), "/te%73t%5f%73%74%72in%67.%68%74m%6c")
+
 
 if __name__ == '__main__':
     unittest.main()
--- url_obfuscator.py
+++ url_obfuscator.py
@@ -1,13 +1,46 @@
 #!/usr/bin/python3
 
-def ipv4_lookup(url):
-    import subprocess
-    cmd = ['ping', '-c', '1', url, '-4']
-    status,res = subprocess.getstatusoutput(' '.join([x for x in cmd]))
-    if status == 0: #0 success, 1 no reply, 2 other error
-        res = res.split()[2][1:-1] #ip addr
-    return status, res
+from random import randint, seed
+from urllib.parse import urlparse, urlunparse
+from socket import gethostbyname
+
+def ip_lookup(url):
+    try:
+        res = gethostbyname(url)
+    except:
+        res = "IP error"
+    return res
+
+def to_oct(n):
+    # TODO arbitrary leading 0
+    return '0{:o}'.format(n)
+def to_hex(n): return '0x{:02x}'.format(n)
+
+def dummy_auth():
+    "add random text before @ as dummy auth"
+    leng = randint(0, 20)
+    str = ""
+    for i in range(leng): str += chr(randint(33,126))
+    return str + '@'
+
+def hext_obfus(text):
+    "obscure text in path by replacing with hex values; apart from /"
+    str = ""
+    for c in text:
+        if c != '/' and randint(0,1):
+            c = '%{:x}'.format(ord(c))
+        str += c
+    return str
 
 
 if __name__ == "__main__":
-    print(ipv4_lookup("www.facebook.oneno"))
+    seed(1)
+    url = "http://github.com/test_path.htm"
+    o = list(urlparse(url))
+    print(o)
+    ip = ip_lookup(o[1])
+    if ip != "IP error":
+        o[1] = ip
+    o[1] = dummy_auth() + o[1]
+    o[2] = hext_obfus(o[2])
+    print(urlunparse(o))
