442a44fecbf0a89c1ae4e76b5c481529b91019a8
F.Fitzgerald <mrffitzgerald@runbox.com>
2018-07-25 14:51:55 +0100

    added argparse, main method, usage

--- url_obfuscator.py
+++ url_obfuscator.py
@@ -1,12 +1,14 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
+import sys
+import argparse
 from random import randint, seed
 from urllib.parse import urlparse, urlunparse
 from socket import gethostbyname
 
-def ip_lookup(url):
+def ip_lookup(hostname):
     try:
-        res = gethostbyname(url)
+        res = gethostbyname(hostname)
     except:
         res = "IP error"
     return res
@@ -15,47 +17,79 @@ def to_oct(n):
     leading_zero = '0' * randint(1,6)
     return '{}{:o}'.format(leading_zero, n)
 
-def to_hex(n): return '0x{:02x}'.format(n)
+def to_hex(n):
+    return '0x{:02x}'.format(n)
 
-def split_ip(ip): return [int(i) for i in ip.split('.')]
+def split_ip(ip_str):
+    return [int(i) for i in ip_str.split('.')]
 
 def ip_dword(ip):
-    "convert rightmost portion of ip to decimal, given full ip 32 bit max"
+    "convert rightmost portion of ip to decimal"
     r = 0
     v = 1
     for i in reversed(ip):
         r += i * v
-        v <<= 8 
+        v <<= 8
     return r
 
 def dummy_auth():
     "add random text before @ as dummy auth"
     leng = randint(0, 20)
-    str = ""
-    for i in range(leng): str += chr(randint(33,126))
-    return str + '@'
+    st = ""
+    for i in range(leng):
+        c = chr(randint(33,126))
+        #some characters noticed not working on chromium
+        if c == '/' or c == '?' or c == '#' or c == '\\': continue
+        st += c
+    return st + '@'
 
 def hext_obfus(text):
     "obscure text in path by replacing with hex values; apart from /"
-    str = ""
+    st = ""
     for c in text:
-        if c != '/' and randint(0,1):
+        if c != '/' and randint(0,1): #50% chance per char
             c = '%{:x}'.format(ord(c))
-        str += c
-    return str
+        st += c
+    return st
 
+def obscure_ip(ip_str):
+    "randomly obscures ip by using oct, hex or decimal"
+    ip = split_ip(ip_str)
+    st = []
+    for i in range(len(ip)):
+        choice = randint(0,2)
+        if choice == 0:
+            st.append(to_oct(ip[i]))
+        elif choice==1:
+            st.append(to_hex(ip[i]))
+        else:
+            st.append(str(ip_dword(ip[i:]))) #rest of ip
+            break
+    return '.'.join(st)
+
+def obfuscate_url(url, change_hostname, add_auth):
+    ob = list(urlparse(url))
+
+    if change_hostname:
+        ip = ip_lookup(ob[1])
+        if ip == "IP error": return ip
+        ob[1] = obscure_ip(ip)
+
+    if add_auth:
+        ob[1] = dummy_auth() + ob[1]
+
+    ob[2] = hext_obfus(ob[2])
+
+    return urlunparse(ob)
+
+def main():
+    parser = argparse.ArgumentParser(description='Obfuscate a url.')
+    parser.add_argument('url', type=str)
+    parser.add_argument('--no_host', help='keep hostname useful due to CDN like cloudflare', action='store_false')
+    parser.add_argument('--no_auth', help='no dummy auth added to hostname', action='store_false')
+    args = parser.parse_args()
+    print(obfuscate_url(args.url, args.no_host, args.no_auth))
+    return 0
 
 if __name__ == "__main__":
-    seed(1)
-    url = "http://github.com/test_path.htm"
-    o = list(urlparse(url))
-    print(o)
-    ip = ip_lookup(o[1])
-    if ip != "IP error":
-        o[1] = ip
-    o[1] = dummy_auth() + o[1]
-    o[2] = hext_obfus(o[2])
-    print(urlunparse(o))
-
-    tt = split_ip("127.0.0.1")
-    print(ip_dword(tt))
+    sys.exit(main())
